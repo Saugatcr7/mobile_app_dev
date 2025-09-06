66c28653e1202b3f4ad523928e8433a5f5781b45
Saugat Khatri <skhatri7@mac.attlocal.net>
2025-09-06 00:38:43 -0500

    First program

---
+++ HelloWorld/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace HelloWorld;
+
+public partial class MainPage : ContentPage
+{
+    
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+
+    private void Button_OnClicked(object? sender, EventArgs e)
+    {
+        if (lblDisplay.Text == "Hello World")
+        {
+            lblDisplay.Text = "That's what I am talking about!";
+        }
+        else
+        {
+            lblDisplay.Text = "Hello World";
+        }
+        
+       
+    }
+}
